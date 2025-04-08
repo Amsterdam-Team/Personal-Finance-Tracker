aa1f2ec2dbdbfd9a18f23c90c1791659f85cb941
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2025-04-08 23:19:36 +0200

    added test cases to cover different edge cases in balance report calculation

--- src/main/kotlin/test/TestCases.kt
+++ src/main/kotlin/test/TestCases.kt
@@ -13,7 +13,41 @@ fun main(){
 //endregion
 
 //region Report Test Cases
-//todo: write all test cases that related with Report here :)
+    check(
+        testName = "1. When there are no transactions in the date range, should return zero income, zero expenses, and zero net balance",
+        expectedResult = "",
+        actualResult = ""
+    )
+
+    check(
+        testName = "2. When only income transactions exist in the date range, should return correct income total and zero expenses",
+        expectedResult = "",
+        actualResult = ""
+    )
+
+    check(
+        testName = "3. When only expense transactions exist in the date range, should return correct expense total and zero income",
+        expectedResult = "",
+        actualResult = ""
+    )
+
+    check(
+        testName = "4. When both income and expense transactions exist, should return correct totals and net balance",
+        expectedResult = "",
+        actualResult = ""
+    )
+
+    check(
+        testName = "5. When start date is after end date, should return null",
+        expectedResult = null,
+        actualResult = ""
+    )
+
+    check(
+        testName = "6. When start or end date is invalid should return null",
+        expectedResult = null,
+        actualResult = ""
+    )
 
 //endregion
 }
