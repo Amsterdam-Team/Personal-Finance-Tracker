e46fcc256b16f73b01ba4ce07a256cbe8bd716d0
omer1998 <omerfaris11@gmail.com>
2025-04-09 05:15:05 +0300

    add data layer structure

---
+++ src/main/kotlin/data/DataSource.kt
@@ -0,0 +1,17 @@
+package data
+
+import models.MonthlySummaryReport
+import models.Response
+import models.Transaction
+import java.time.LocalDate
+
+interface TransactionDataSource {
+    fun addTransaction(transaction: Transaction): Response<String>
+    fun viewTransaction(transactionName: String): Response<Transaction>
+    fun deleteTransaction(transactionName: String): Response<String>
+    fun editTransaction(transactionName: String, newTransaction: Transaction): Response<String>
+}
+interface ReportDataSource {
+    fun getMonthlySummaryReport(month: LocalDate ): Response<MonthlySummaryReport>
+    fun getBalanceReport() : Response<String> // or may be other representative data of balance report
+}
---
+++ src/main/kotlin/data/FileDataSource.kt
@@ -0,0 +1,32 @@
+package data
+
+import models.MonthlySummaryReport
+import models.Response
+import models.Transaction
+import java.time.LocalDate
+
+class FileDataSource(val fileName: String) : ReportDataSource, TransactionDataSource{
+    override fun getMonthlySummaryReport(month: LocalDate): Response<MonthlySummaryReport> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getBalanceReport(): Response<String> {
+        TODO("Not yet implemented")
+    }
+
+    override fun addTransaction(transaction: Transaction): Response<String> {
+        TODO("Not yet implemented")
+    }
+
+    override fun viewTransaction(transactionName: String): Response<Transaction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteTransaction(transactionName: String): Response<String> {
+        TODO("Not yet implemented")
+    }
+
+    override fun editTransaction(transactionName: String, newTransaction: Transaction): Response<String> {
+        TODO("Not yet implemented")
+    }
+}
