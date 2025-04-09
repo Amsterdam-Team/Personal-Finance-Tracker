347856152ec90a9af11294cbe2c45c33297eef73
omer1998 <omerfaris11@gmail.com>
2025-04-09 05:41:36 +0300

    add another option to consider of response class

--- src/main/kotlin/models/Response.kt
+++ src/main/kotlin/models/Response.kt
@@ -1,3 +1,9 @@
 package models
 
 class Response<T> (val status: Boolean, val result:T)
+
+// another Response class might be more efficient using sealed class
+//sealed class Response<out T>{
+//    data class Success<out T>(val result: T) : Response<T>()
+//    data class Error(val result: String) : Response<Nothing>()
+//}
