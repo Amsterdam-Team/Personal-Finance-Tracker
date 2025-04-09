9f78fdd86b00f9e508494bcfa6162758460f39a2
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2025-04-09 13:33:21 +0200

    - finish edit transaction test cases

--- src/main/kotlin/test/TestCases.kt
+++ src/main/kotlin/test/TestCases.kt
@@ -2,8 +2,78 @@ package test
 
 
 fun main(){
+
 //region Transactions Test Cases
-//todo: write all test cases that related with transactions here :)
+
+    //region Edit Transaction Test Cases
+
+     check(
+        testName = "When all inputs are valid and transaction exists, then return true",
+        result = false,
+        acceptedResult = false
+    )
+    check(
+        testName = "when transaction type is expenses and amount more than user balance return false",
+        result = false,
+        acceptedResult = false
+    )
+    check(
+        testName = "when amount equals zero or negative number return false",
+        result = false,
+        acceptedResult = false
+    )
+    check(
+        testName = "when amount is something else number should return false",
+        result = false,
+        acceptedResult = false
+    )
+    check(
+        testName = "when id of transaction is invalid should return false",
+        result = false,
+        acceptedResult = false
+    )
+    check(
+        testName = "when description is invalid like (numbers,special characters) should return false",
+        result = false,
+        acceptedResult = false
+    )
+    check(
+        testName = "when description is empty should return true",
+        result = true,
+        acceptedResult = true
+    )
+    check(
+        testName = "when date is invalid should return false",
+        result = false,
+        acceptedResult = false
+    )
+    // set today date
+    check(
+        testName = "when date is empty should return true",
+        result = true,
+        acceptedResult = true
+    )
+    check(
+        testName = "when transaction type is empty should return false",
+        result = false,
+        acceptedResult = false
+    )
+    check(
+        testName = "when transaction type not supported should return false",
+        result = false,
+        acceptedResult = false
+    )
+    check(
+        testName = "when category type is empty should return false",
+        result = false,
+        acceptedResult = false
+    )
+    check(
+        testName = "when category type is invalid return false",
+        result = false,
+        acceptedResult = false
+    )
+    //endregion
 
 //endregion
 
