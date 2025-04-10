7eff7d62db1bb6459b364f89b22fd0fea92e6e7e
raouf-kamel <raoufkamel2013@gmail.com>
2025-04-10 15:57:06 +0300

    delete ActionStatus to ResultStatus

--- src/main/kotlin/utils/ActionStatus.kt
+++
@@ -1,8 +0,0 @@
-package utils
-
-sealed class ActionStatus<out T> {
-    data class Success<out T>(val data: T) : ActionStatus<T>()
-    data class Error(val errorMessage: String) : ActionStatus<Nothing>()
-    object InProgress : ActionStatus<Nothing>()
-    object Empty : ActionStatus<Nothing>()
-}
