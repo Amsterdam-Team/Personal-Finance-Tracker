a1bf43407704bd44dd76003e61f285bc5bc252dc
HendSayed25 <hendsayed853@gmail.com>
2025-04-10 20:02:33 +0200

    add the logic of edit category function

--- src/main/kotlin/managers/CategoryManager.kt
+++ src/main/kotlin/managers/CategoryManager.kt
@@ -1,9 +1,21 @@
 package managers
 import models.Category
 import saver.IFileManager
+import utils.ResultStatus
 
 class CategoryManager(private val fileManager: IFileManager) {
 
+
+    fun editCategory(category: Category):ResultStatus<Boolean>{
+        val result = checkEditCategory(category.name,category.id)
+        if(!result)return ResultStatus.Error("Invalid Category Name Or Id")
+
+        //delete the old value and save the new
+        //fileManager.deleteObjectById(category.id)
+        fileManager.saveObject(category)
+
+        return ResultStatus.Success(true)
+    }
 
     private fun checkAddCategory(categoryName:String):Boolean{
         return false
@@ -15,6 +27,6 @@ class CategoryManager(private val fileManager: IFileManager) {
         return false
     }
     private fun checkEditCategory(categoryName:String,categoryId:Any):Boolean{
-        return false
+        return ((categoryName.matches(Regex("^[A-Za-z]+$"))) && (categoryId is Int && categoryId > 0))
     }
 }
