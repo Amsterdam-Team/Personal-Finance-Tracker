9db4781c7ea1e2e49ae14bf637b81ec1b3abd6f9
Mohamed Hassan <127283755+mohamedd-hassan@users.noreply.github.com>
2025-04-10 18:14:35 +0200

    added parser to parse data objects and implemented the file manager

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,19 +12,18 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     val fileManager: FileManagerImpl = FileManagerImpl()
     val category = Category(5,"food")
-//    fileManager.saveObject(Transaction(
-//        id = 5,
-//        amount = 5.0,
-//        description = "",
-//        category = category,
-//        date = LocalDate.now(),
-//        type = TransactionType.INCOME
-//    ))
-//    fileManager.saveObject(category)
+    fileManager.saveObject(Transaction(
+        id = 5,
+        amount = 5.0,
+        description = "",
+        category = category,
+        date = LocalDate.now(),
+        type = TransactionType.INCOME
+    ))
+    fileManager.saveObject(category)
 
     println(fileManager.getAllObjects(Category::class))
-    fileManager.deleteObjectById(8)
-    println(fileManager.getAllObjects(Category::class))
+    println(fileManager.getAllObjects(Transaction::class))
 
 }
 
