356ec339fb0fa01458f461e6ca88f54e618d7745
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2025-04-11 18:15:26 +0200

    use validators class

--- src/main/kotlin/managers/TransactionManager.kt
+++ src/main/kotlin/managers/TransactionManager.kt
@@ -1,14 +1,15 @@
 package managers
 
+import Validators.isValidCategory
 import Validators.isValidDate
 import Validators.isValidDescription
+import Validators.isValidID
 import Validators.isValidInputAmount
 import Validators.isValidTransactionType
 import com.sun.jdi.IntegerType
 import models.Category
 import models.Transaction
 import models.TransactionType
-import models.TransactionViewResult
 import saver.IFileManager
 import utils.ResultStatus
 import java.text.DateFormat
@@ -59,10 +60,10 @@ class TransactionManager(private val fileManager: IFileManager) {
         if (listOf(
                 isValidID(transaction.id),
                 isValidDescription(transaction.description),
-                isValidaType(transaction.type.toString()),
+                isValidTransactionType(transaction.type.toString()),
                 isValidDate(transaction.date.toString()),
                 isValidCategory( transaction.category.name),
-                isValidAmount(transaction.amount)
+                isValidInputAmount(transaction.amount.toString())
             ).all { it == ResultStatus.Success("success") }
         ) {
             fileManager.deleteObjectById(transaction.id,Transaction::class.java)
@@ -76,55 +77,5 @@ class TransactionManager(private val fileManager: IFileManager) {
     }
 }
 
-fun isValidID(id: UUID): ResultStatus<String> {
-
-
-
-    if (id.toString().contains(" ") || id.toString().isBlank())
-        return ResultStatus.Error("Invalid Id")
-    return ResultStatus.Success("success")
-}
-
-fun isValidAmount(amount: Double?): ResultStatus<String> {
-
-
-    if (amount != null && amount > 0.0)
-        return ResultStatus.Success("success")
-    return ResultStatus.Error("Invalid Amount")
-}
-
-fun isValidCategory (category: String): ResultStatus<String> {
-    if (category.isBlank())
-        return ResultStatus.Error("Invalid Category")
-    return ResultStatus.Success("success")
-
-}
-
-fun isValidaType(type: String): ResultStatus<String> {
-    if (enumValues<TransactionType>().any { it.name.equals(type, ignoreCase = true) })
-        return ResultStatus.Success("success")
-    else
-        return ResultStatus.Error("Invalid Type")
-}
-
-fun isValidDescription(input: String): ResultStatus<String> {
-    val regex = Regex("^(?=.*[a-zA-Z]).*$")
-    if (regex.matches(input) || input.isBlank())
-        return ResultStatus.Success("success")
-    return ResultStatus.Error("Invalid Description")
-}
-
-fun isValidDate(date: String): ResultStatus<String> {
-    val formatter = DateTimeFormatter.ofPattern("dd/mm/yyyy")
-    if (date.isNotBlank()) {
-        return try {
-            LocalDate.parse(date, formatter)
-            ResultStatus.Success("success")
-        } catch (e: DateTimeParseException) {
-            ResultStatus.Error("Invalid Date")
-        }
-    }
-    return ResultStatus.Error("Invalid Date")
-}
 
 
