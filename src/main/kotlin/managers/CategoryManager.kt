16c7d1c8b9aca601e3329c6ddb71fd51e50e3266
HendSayed25 <hendsayed853@gmail.com>
2025-04-11 22:09:45 +0200

    update edit category validation return type

--- src/main/kotlin/managers/CategoryManager.kt
+++ src/main/kotlin/managers/CategoryManager.kt
@@ -22,14 +22,14 @@ class CategoryManager(private val fileManager: IFileManager) {
         }
     }
 
-    fun editCategory(category: Category): ResultStatus<Boolean> {
+    fun editCategory(category: Category): ResultStatus<String> {
         fileManager.getObjectById(category.id.toString(), Category::class.java)
             ?: return ResultStatus.Error("This Id Not Found In File")
 
         fileManager.deleteObjectById(category.id, Category::class.java)
         fileManager.saveObject(category)
 
-        return ResultStatus.Success(true)
+        return ResultStatus.Success("Success Editing")
     }
 
     fun deleteCategoryById(id: String): ResultStatus<String> {
