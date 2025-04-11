2e717c1fe28183314459388a71491f981f120dc4
Mohammad Ashraf <mohamedashrf155@gmail.com>
2025-04-11 16:24:01 +0200

    merge delete-category and develop branches — added delete and edit functions

--- src/main/kotlin/managers/CategoryManager.kt
+++ src/main/kotlin/managers/CategoryManager.kt
@@ -31,4 +31,14 @@ class CategoryManager(private val fileManager: IFileManager) {
 
         return null
     }
+
+    fun editCategory(category: Category):ResultStatus<Boolean>{
+        fileManager.getObjectById(category.id.toString(),Category::class.java)
+            ?: return ResultStatus.Error("This Id Not Found In File")
+
+        fileManager.deleteObjectById(category.id,Category::class.java)
+        fileManager.saveObject(category)
+
+        return ResultStatus.Success(true)
+    }
 }
