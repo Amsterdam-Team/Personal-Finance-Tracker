0c18d04ca8534bf0a6b42ec6cb4daf603421f641
Mohammad Ashraf <mohamedashrf155@gmail.com>
2025-04-11 16:32:02 +0200

    conflict resolved

--- src/main/kotlin/managers/CategoryManager.kt
+++ src/main/kotlin/managers/CategoryManager.kt
@@ -18,4 +18,14 @@ class CategoryManager(private val fileManager: IFileManager) {
             ResultStatus.Success(categories)
         }
     }
+
+    fun editCategory(category: Category): ResultStatus<Boolean> {
+        fileManager.getObjectById(category.id.toString(), Category::class.java)
+            ?: return ResultStatus.Error("This Id Not Found In File")
+
+        fileManager.deleteObjectById(category.id, Category::class.java)
+        fileManager.saveObject(category)
+
+        return ResultStatus.Success(true)
+    }
 }
