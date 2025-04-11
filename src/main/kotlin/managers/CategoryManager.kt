a45d3280a2585cc7c3f8e7d015df0065675b4202
Mohammad Ashraf <mohamedashrf155@gmail.com>
2025-04-11 15:55:50 +0200

    deleted redundant check functions

--- src/main/kotlin/managers/CategoryManager.kt
+++ src/main/kotlin/managers/CategoryManager.kt
@@ -18,20 +18,4 @@ class CategoryManager(private val fileManager: IFileManager) {
             ResultStatus.Success(categories)
         }
     }
-
-    private fun checkAddCategory(categoryName: String): Boolean {
-        return false
-    }
-
-    private fun checkDeleteCategory(categoryId: Any?): Boolean {
-        return false
-    }
-
-    private fun checkViewCategory(categories: List<Category>): Boolean {
-        return false
-    }
-
-    private fun checkEditCategory(categoryName: String, categoryId: Any): Boolean {
-        return false
-    }
 }
