15ecb185490c17966cdaaed457462025f18d2126
Mohammad Ashraf <mohamedashrf155@gmail.com>
2025-04-11 15:32:07 +0200

    add viewCategories logic in CategoryManager

--- src/main/kotlin/managers/CategoryManager.kt
+++ src/main/kotlin/managers/CategoryManager.kt
@@ -1,20 +1,37 @@
 package managers
+
 import models.Category
 import saver.IFileManager
+import utils.ResultStatus
 
 class CategoryManager(private val fileManager: IFileManager) {
 
+    fun viewCategories(): ResultStatus<List<Category>> {
+        val categories = fileManager.getAllObjects(Category::class.java)
+        return validateViewCategories(categories)
+    }
 
-    private fun checkAddCategory(categoryName:String):Boolean{
+    private fun validateViewCategories(categories: List<Category>): ResultStatus<List<Category>> {
+        return if (categories.isEmpty()) {
+            ResultStatus.Empty("No categories found!")
+        } else {
+            ResultStatus.Success(categories)
+        }
+    }
+
+    private fun checkAddCategory(categoryName: String): Boolean {
         return false
     }
-    private fun checkDeleteCategory(categoryId:Any?):Boolean{
+
+    private fun checkDeleteCategory(categoryId: Any?): Boolean {
         return false
     }
-    private fun checkViewCategory(categories:List<Category>):Boolean{
+
+    private fun checkViewCategory(categories: List<Category>): Boolean {
         return false
     }
-    private fun checkEditCategory(categoryName:String,categoryId:Any):Boolean{
+
+    private fun checkEditCategory(categoryName: String, categoryId: Any): Boolean {
         return false
     }
 }
