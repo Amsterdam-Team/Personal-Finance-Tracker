1f3e230d19abff22cca8731c1def12d79825dd05
HendSayed25 <hendsayed853@gmail.com>
2025-04-11 15:23:15 +0200

    edit the editCategory Function

--- src/main/kotlin/managers/CategoryManager.kt
+++ src/main/kotlin/managers/CategoryManager.kt
@@ -2,24 +2,16 @@ package managers
 import models.Category
 import saver.IFileManager
 import utils.ResultStatus
-import java.util.*
 
 class CategoryManager(private val fileManager: IFileManager) {
 
-
     fun editCategory(category: Category):ResultStatus<Boolean>{
-        val result = isValidCategoryForEdit(category.name,category.id)
-        if(!result)return ResultStatus.Error("Invalid Category Name Or Id")
+        fileManager.getObjectById(category.id.toString(),Category::class.java)
+            ?: return ResultStatus.Error("This Id Not Found In File")
 
         fileManager.deleteObjectById(category.id,Category::class.java)
         fileManager.saveObject(category)
 
         return ResultStatus.Success(true)
     }
-
-    private fun isValidCategoryForEdit(categoryName:String, categoryId:Any):Boolean{
-        return ((categoryName.matches(Regex("^[A-Za-z]+$"))) && (categoryId is UUID))
-    }
-
-
 }
