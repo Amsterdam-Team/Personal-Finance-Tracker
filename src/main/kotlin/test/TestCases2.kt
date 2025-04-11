b71684afb929ca958b0b9cb843be6d7416582966
3bnaby <mahmoud.mohamad68@gmail.com>
2025-04-12 00:28:18 +0200

    improve test cases scenarios

--- src/main/kotlin/test/TestCases2.kt
+++ src/main/kotlin/test/TestCases2.kt
@@ -5,6 +5,10 @@ import managers.TransactionManager
 import saver.FileManagerImpl
 import utils.ResultStatus
 import utils.Validator
+import utils.Validator.isValidDate
+import utils.Validator.isValidInput
+import utils.Validator.isValidInputAmount
+import utils.Validator.isValidTransactionType
 import java.util.*
 
 fun main(){
@@ -13,49 +17,96 @@ fun main(){
     val transactionManager = TransactionManager(fileManager)
     val categoryManager = CategoryManager(fileManager)
 
+//region add transaction test cases
+    check(
+        testName = "when amount is empty should return false",
+        result = isValidInputAmount(""),
+        acceptedResult = ResultStatus.Empty("please enter an amount")
+    )
+    check(
+        testName = "when amount is less than or equal zero number should return false",
+        result = isValidInputAmount("0"),
+        acceptedResult = ResultStatus.Error("amount number should be more than zero")
+    )
+    check(
+        testName = "when amount is letters or special characters should return false",
+        result = isValidInputAmount("a&"),
+        acceptedResult = ResultStatus.Error("amount number shouldn't contain any letters or special characters")
+    )
+    check(
+        testName = "when description is invalid like (containing only numbers,special characters) should return false",
+        result = isValidInput("1213$%#"),
+        acceptedResult = ResultStatus.Error("input shouldn't be numbers or special characters only it must have a letters")
+    )
+    check(
+        testName = "when description is empty should return true",
+        result = isValidInput(""),
+        acceptedResult = ResultStatus.Empty("please enter an input")
+    )
+    check(
+        testName = "when date is invalid should return false",
+        result = isValidDate("1-2-2024"),
+        acceptedResult = ResultStatus.Error("please enter a valid date with that format : yyyy-MM-dd")
+    )
+    check(
+        testName = "when date is empty should return false",
+        result = isValidDate(""),
+        acceptedResult = ResultStatus.Empty("please enter the date")
+    )
+    check(
+        testName = "when transaction type is empty should return false",
+        result = isValidTransactionType(""),
+        acceptedResult = ResultStatus.Error("please enter a transaction type ")
+    )
+    check(
+        testName = "when transaction type isn't one of these(INCOME,EXPENSE) should return false",
+        result = isValidTransactionType("ahmed"),
+        acceptedResult = ResultStatus.Error("please enter one of these types only (INCOME,EXPENSE)")
+    )
+    //endregion
 
 
     // region validators test cases
     check(
         testName = "when amount of existing transaction equals zero or negative number return false",
-        result = Validator.isValidInputAmount("0.0"),
+        result = isValidInputAmount("0.0"),
         acceptedResult = ResultStatus.Error("amount number should be more than zero")
     )
     check(
         testName = "when amount is something else number should return false",
-        result = Validator.isValidInputAmount("iuhiuhis"),
+        result = isValidInputAmount("iuhiuhis"),
         acceptedResult = ResultStatus.Error("amount number shouldn't contain any letters or special characters")
     )
 
     check(
         testName = "when description is invalid like (numbers,special characters) should return false",
-        result = Validator.isValidInput("$#%@"),
+        result = isValidInput("$#%@"),
         acceptedResult = ResultStatus.Error("input shouldn't be numbers or special characters only it must have a letters")
     )
     check(
         testName = "when description is empty should return false",
-        result = Validator.isValidInput(""),
+        result = isValidInput(""),
         acceptedResult = ResultStatus.Empty("please enter an input")
     )
     check(
         testName = "when date is invalid should return false",
-        result = Validator.isValidDate("145224"),
+        result = isValidDate("145224"),
         acceptedResult = ResultStatus.Error("please enter a valid date with that format : yyyy-MM-dd")
     )
 
     check(
         testName = "when transaction type is empty should return false",
-        result = Validator.isValidInput(""),
+        result = isValidInput(""),
         acceptedResult = ResultStatus.Empty("please enter an input")
     )
     check(
         testName = "when category type is empty should return ",
-        result = Validator.isValidInput(""),
+        result = isValidInput(""),
         acceptedResult = ResultStatus.Empty("please enter an input")
     )
     check(
         testName = "when category type is invalid return false",
-        result = Validator.isValidInput("%#&*"),
+        result = isValidInput("%#&*"),
         acceptedResult = ResultStatus.Error("input shouldn't be numbers or special characters only it must have a letters")
     )
     //endregion
